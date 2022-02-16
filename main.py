9d42a97f734872b3fc808ef950884c1aba9924f3
Wodey <54207104+Wodey@users.noreply.github.com>
2022-02-16 16:56:18 +0300

    + board initialization + render function

--- main.py
+++ main.py
@@ -1,16 +1,51 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+horizontal = {"a", "b", "c", "d", "e", "f", "g", "h"}
+vertical = ["1", "2", "3", "4", "5", "6", "7", "8"]
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+class Chess:
+    def __init__(self):
+        self.chess_board = {
+            k + v: None for k in horizontal for v in vertical
+        }
+
+        for i in horizontal:
+            self.chess_board[i + "2"] = "P"
+            self.chess_board[i + "7"] = "p"
+
+        self.chess_board['a1'] = "R"
+        self.chess_board['b1'] = "N"
+        self.chess_board['c1'] = "B"
+        self.chess_board['d1'] = "Q"
+        self.chess_board['e1'] = "K"
+        self.chess_board['f1'] = "B"
+        self.chess_board['g1'] = "N"
+        self.chess_board['h1'] = "R"
+
+        for i in horizontal:
+            self.chess_board[i + "8"] = self.chess_board[i + "1"].lower()
+
+    def restart(self):
+        self.__init__()
+
+    def render_chessboard(self):
+        print('  ', end=" ")
+        for k in sorted(list(horizontal)):
+            print(k + "", end=" ")
+        print(" ")
+        for i in sorted(list(vertical)):
+            print(i, end="  ")
+            for k in sorted(list(horizontal)):
+                if self.chess_board[k+i]:
+                    print(self.chess_board[k + i], end=" ")
+                else:
+                    print(".", end=" ")
+            print(" " + i)
+        print('  ', end=" ")
+        for k in sorted(list(horizontal)):
+            print(k + "", end=" ")
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+if __name__ == "__main__":
+    cb = Chess()
+    cb.render_chessboard()
